5d9ed80c26be492ee9728423cc2dd60d511c1c2f
Hieu Tran <hmtran.ae@gmail.com>
2019-03-28 13:52:02 -0400

    implementation to hasPairWithSum

--- pairSumToTarget.js
+++ pairSumToTarget.js
@@ -1,3 +1,18 @@
+// [1, 2, 3, 9] sum = 8
+// [1, 2, 4, 4] sum = 8
+
+// naive approach
+function hasPairWithSum(arr, sum) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === sum) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 console.log(hasPairWithSum2([1, 2, 3, 9], 8));
 console.log(hasPairWithSum2([1, 2, 4, 3], 8));
 
