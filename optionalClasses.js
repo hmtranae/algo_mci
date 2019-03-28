e28bd24437c84192a42a35c98821496f9b869822
Hieu Tran <hmtran.ae@gmail.com>
2019-03-28 15:46:04 -0400

    context vs scope in js

--- optionalClasses.js
+++ optionalClasses.js
@@ -17,6 +17,23 @@ object2.value; // 15
 object3.value; // 10
 
 // context vs scope
+function b() {
+    // inner scope
+    let a = 4;
+}
+// global scope
 
+// context tells you where you are in the object
+console.log(this); // window object
+// this refers to what object you are inside of
+function a() {
+    console.log(this); // still the window object
+}
+
+const object4 = {
+    a: function() {
+        console.log(this); // object4
+    }
+}
 
 // instantiation
