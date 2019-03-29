9e8df8723934aa910261e16cbee7332e124700f9
Hieu Tran <hmtran.ae@gmail.com>
2019-03-29 16:14:50 -0400

    hash table keys method

--- hashFunctionClass.js
+++ hashFunctionClass.js
@@ -1,7 +1,6 @@
 class HashTable {
     constructor(size) {
         this.data = new Array(size);
-        // [['grapes', 10000]]
     }
 
     _hash(key) {
@@ -25,8 +24,21 @@ class HashTable {
         const index = this._hash(key);
         return this.data[index][1];
     }
+
+    keys() {
+        const keysArray = [];
+        for (let i = 0; i < this.data.length; i++) {
+            if (this.data[i]) {
+                keysArray.push(this.data[i][0])
+            }
+        }
+        return keysArray;
+    }
 }
 
 const myHashTable = new HashTable(50);
 console.log(myHashTable.set('grapes', 10000));
+console.log(myHashTable.set('apples', 54));
+console.log(myHashTable.set('oranges', 2));
 console.log(myHashTable.get('grapes'));
+console.log(myHashTable.keys());
