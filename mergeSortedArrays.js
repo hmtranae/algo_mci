48830d1e0e289f303f48b4fbdbf4cfad382d2746
Hieu Tran <hmtran.ae@gmail.com>
2019-03-28 23:55:04 -0400

    merge two sorted arrays

--- mergeSortedArrays.js
+++ mergeSortedArrays.js
@@ -1,5 +1,14 @@
 function mergeSortedArrays(arr1, arr2) {
     let sortedArray = [];
+
+    if (arr1.length === 0) {
+        return arr2;
+    }
+
+    if (arr2.length === 0) {
+        return arr1;
+    }
+
     while (arr1.length && arr2.length) {
         if (arr1[0] < arr2[0]) {
             sortedArray.push(arr1.shift());
