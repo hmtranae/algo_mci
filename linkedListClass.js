42916728e27307ad8e058d9ee83cc98da56f9cb1
Hieu Tran <hmtran.ae@gmail.com>
2019-03-29 23:03:26 -0400

    added tail and length prop in constructor of linkedlist

--- linkedListClass.js
+++ linkedListClass.js
@@ -3,6 +3,18 @@ class LinkedList {
         this.head = {
             value: value,
             next: null
-        } 
+        }
+        this.tail = this.head;
+        this.length = 1;
     }
+
+    append(value) {
+
+    }
+
 }
+
+const myLinkedList = new LinkedList(10);
+// myLinkedList.append(5);
+// myLinkedList.append(16);
+console.log(myLinkedList);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,23 +1,20 @@
-// Given array = [2, 5, 1, 2, 3, 5, 1, 2, 4];
-// Returns 2
-
-// Given array = [2, 3, 4, 5];
-// Returns undefined
-
-function firstRepeatingChar(array) {
-    let hashTable = {};
-    for (let num of array) {
-        if (hashTable[num]) {
-            return num;
-        } else {
-            hashTable[num] = true
+class LinkedList {
+    constructor(value) {
+        this.head = {
+            value: value,
+            next: null
         }
+        this.tail = this.head;
+        this.length = 1;
     }
-    return undefined;
+
+    append(value) {
+
+    }
+
 }
 
-let array = [2, 5, 1, 2, 3, 5, 1, 2, 4];
-let array2 = [2, 3, 4, 5];
-
-console.log(firstRepeatingChar(array));
-console.log(firstRepeatingChar(array2));
+const myLinkedList = new LinkedList(10);
+// myLinkedList.append(5);
+// myLinkedList.append(16);
+console.log(myLinkedList);
