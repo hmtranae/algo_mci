a81aca0c2f0ec16a4ceb74d54e1f15476528eb0d
Hieu Tran <hmtran.ae@gmail.com>
2019-03-29 23:46:35 -0400

    printlist method

--- linkedListClass.js
+++ linkedListClass.js
@@ -30,11 +30,28 @@ class LinkedList {
         }
         this.length++;
     }
+
+    printList() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return array;
+    }
+
+    insert(index, value) {
+
+    }
+
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
-// 10 --> 5 --> 16
+myLinkedList.printList();
+myLinkedList.insert(2, 99);
+// 1 --> 10 --> 99 --> 5 --> 16
 console.log(myLinkedList);
